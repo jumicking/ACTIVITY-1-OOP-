57bb868c384d167daf6e509bcff3d88a92677f7c
Jumayca Malilong <malilongjumayca@g.cjc.edu.ph>
2025-02-13 19:21:24 +0800

    Add files via upload

---
+++ ArithmeticProblem2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arithmetic.problem.pkg2;
+
+/**
+ *
+ * @author MALILONG_CpE121
+ */
+public class ArithmeticProblem2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+
+      Sum series = new Sum (89);
+       series.displayResult();
+    
+    }
+    
+}
+    
+  
+
